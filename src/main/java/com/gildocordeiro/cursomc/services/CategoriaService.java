88d8d852d1210a8f94cb6300201b708e75840884
gildo_cordeiro <gildo_cordeiro@B1132014044683.imd.local>
2020-02-21 15:25:12 -0300

    Ajustes no endpoint

--- src/main/java/com/gildocordeiro/cursomc/services/CategoriaService.java
+++ src/main/java/com/gildocordeiro/cursomc/services/CategoriaService.java
@@ -8,14 +8,17 @@ import org.springframework.stereotype.Service;
 import com.gildocordeiro.cursomc.domain.Categoria;
 import com.gildocordeiro.cursomc.repositories.CategoriaRepository;
 
+import com.gildocordeiro.cursomc.services.exception.ObjectNotFoundException;
+
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> categoria = repository.findById(id);
-	    return categoria.orElse(null);
+		return categoria.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id));
 	}
 }
