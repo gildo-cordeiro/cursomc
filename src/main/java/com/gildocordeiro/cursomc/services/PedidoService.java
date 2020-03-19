6446f0dbdc38458974524cd31adbbea3d12a9cae
gildo-cordeiro <gildo.duartec@gmail.com>
2020-03-19 14:44:27 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/gildocordeiro/cursomc/services/PedidoService.java
@@ -0,0 +1,22 @@
+package com.gildocordeiro.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gildocordeiro.cursomc.domain.Pedido;
+import com.gildocordeiro.cursomc.repositories.PedidoRepository;
+import com.gildocordeiro.cursomc.services.exception.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> pedido = pedidoRepository.findById(id);
+		return pedido.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! id do tipo: "+Pedido.class.getName()));
+	}
+}
